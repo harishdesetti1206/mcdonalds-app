4df4b89741a2168787584717f7a6254d689217ae
Harish Desetti <hdesetti@sodimac.cl>
2022-07-25 16:46:46 -0400

    feat: add helm install

---
+++ demoapp-install.tf
@@ -0,0 +1,27 @@
+resource "kubernetes_namespace" "demoapp" {
+  metadata {
+    name = "demoapp"
+    labels = {}
+  }
+}
+
+resource "helm_release" "demoapp" {
+  name             = "demoapp"
+  chart            = "./demoapp"
+  namespace        = kubernetes_namespace.demoapp.metadata[0].name
+  create_namespace = false
+  recreate_pods    = false
+  reuse_values     = false
+  reset_values     = true
+  force_update     = false
+  wait             = true
+
+#   set {
+#     name  = ""
+#     value = ""
+#   }
+
+#   depends_on = [
+    
+#   ]
+}
