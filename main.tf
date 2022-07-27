5ee509a9ca2005ce5334118f004589c55a0e6621
Harish Desetti <hdesetti@sodimac.cl>
2022-07-27 15:56:24 -0400

    fix: fixing connection error to k8s cluster

--- main.tf
+++ main.tf
@@ -18,5 +18,5 @@ terraform {
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  config_context = "k8s-autoscaling-cluster"
+  config_context = "arn:aws:eks:us-east-1:286824333895:cluster/k8s-autoscaling-cluster"
 }
