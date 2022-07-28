c874044f8924b2ef367452f2e70478f251322ef5
Harish Desetti <hdesetti@sodimac.cl>
2022-07-27 20:05:04 -0400

    feat: add s3 backend

--- main.tf
+++ main.tf
@@ -15,9 +15,17 @@ terraform {
     }
   }
   required_version = ">= 1.1.2"
+
+  backend "s3" {
+    bucket = "hdesettistatefiles"
+    key    = "k8s/cluster/k8s-autoscaling-cluster/mcdonaldapp/key/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "dynamodb-state-locking-mcdonaldapp"
+  }
 }
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "arn:aws:eks:us-east-1:286824333895:cluster/k8s-autoscaling-cluster"
 }
+
